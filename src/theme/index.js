096e69e208c90f2710e35c0c231ebc9127a4f765
Thanh Minh <thanhgamedev@gmail.com>
2021-10-21 15:26:11 +0700

    feat(chakra): custom colors theme

---
+++ src/theme/index.js
@@ -0,0 +1,29 @@
+import { extendTheme } from '@chakra-ui/react'
+
+const colors = {
+  primary: {
+    1: '#3B71FE',
+    '1-variant': '#3772FF',
+    2: '#8BC5E5',
+    3: '#92A5EF',
+    4: '#58C27D',
+  },
+  secondary: {
+    1: '#A4CDE3',
+    2: '#E4D7CF',
+    3: '#FFD166',
+    4: '#FA8F54',
+  },
+  neutrals: {
+    1: '#141416',
+    2: '#23262F',
+    3: '#353945',
+    4: '#777E90',
+    5: '#B1B5C3',
+    6: '#E6E8EC',
+    7: '#F4F5F6',
+    8: '#FCFCFD',
+  },
+}
+
+export const theme = extendTheme({ colors })
