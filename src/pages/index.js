f86e432ee4d89b19eb23b70304b3f0b98708442f
Thanh Minh <thanhgamedev@gmail.com>
2021-10-24 22:19:05 +0700

    feat(layouts-default): add default layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,5 @@
 import { Button, Box, Text } from '@chakra-ui/react'
 
 export default function Index() {
-  return (
-    <Box>
-      <Button size="medium" disabled>
-        ABC
-      </Button>
-      <Button size="small">ABC</Button>
-      <Button size="medium" variant="light">
-        ABC
-      </Button>
-      <Button size="small" variant="light">
-        ABC
-      </Button>
-    </Box>
-  )
+  return <Box></Box>
 }
