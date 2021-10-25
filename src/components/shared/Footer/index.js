ef4bd02acc4ddd2b832e2965ce4890fa214f9686
Thanh Minh <thanhgamedev@gmail.com>
2021-10-25 14:46:51 +0700

    feat(component-shared): add footer

--- src/components/shared/Footer/index.js
+++ src/components/shared/Footer/index.js
@@ -1,5 +1,77 @@
-const Footer = () => {
-  return <></>
+import { Stack, Text, Divider } from '@chakra-ui/react'
+import Link from 'next/link'
+import Image from 'next/image'
+
+function Menu({ menu }) {
+  return (
+    <Stack spacing="8px">
+      <Text textStyle="body-1-bold">{menu.title}</Text>
+      {menu.data.map((content, index) => (
+        <Text
+          key={index}
+          color="neutrals.4"
+          _hover={{ color: 'neutrals.1' }}
+          transition="color 0.15s ease-in-out"
+        >
+          <Link {...content.linkProps}>{content.title}</Link>
+        </Text>
+      ))}
+    </Stack>
+  )
+}
+
+const Footer = ({ logoImageSrc, copyright }) => {
+  const menuData = [
+    {
+      title: 'Company',
+      data: [
+        { title: 'About us', linkProps: { href: '/about' } },
+        { title: 'Blog', linkProps: { href: '/blog' } },
+        { title: 'Press', linkProps: { href: '/press' } },
+        { title: 'Contact Us', linkProps: { href: '/contact' } },
+      ],
+    },
+    {
+      title: 'Support',
+      data: [
+        { title: 'Help Center', linkProps: { href: '/helpcenter' } },
+        { title: 'Report', linkProps: { href: '/report' } },
+      ],
+    },
+    {
+      title: 'Legal',
+      data: [
+        { title: 'Cookies Policy', linkProps: { href: '/policy' } },
+        { title: 'Privacy Policy', linkProps: { href: '/privacy' } },
+        { title: 'Terms of Service', linkProps: { href: '/tos' } },
+      ],
+    },
+  ]
+  return (
+    <Stack
+      px={{ mobile: '40px', tablet: '80px', desktop: '160px' }}
+      pt={{ mobile: '64px', tablet: '80px' }}
+      bg="neutrals.8"
+      spacing="24px"
+    >
+      <Stack direction="row" justify="space-between" align="start">
+        <Stack>
+          <Image
+            src={logoImageSrc}
+            alt="Logo"
+            width={106}
+            height={38}
+            unoptimized
+          />
+        </Stack>
+        <Menu menu={menuData[0]} />
+        <Menu menu={menuData[1]} />
+        <Menu menu={menuData[2]} />
+      </Stack>
+      <Divider colorScheme="neutrals.1" />
+      <Text textAlign="center">{copyright}</Text>
+    </Stack>
+  )
 }
 
 export default Footer
--- src/layouts/DefaultLayout.js
+++ src/layouts/DefaultLayout.js
@@ -1,11 +1,16 @@
 import { Box } from '@chakra-ui/react'
 import Navbar from '@/components/shared/Navbar'
+import Footer from '@/components/shared/Footer'
 
 const DefaultLayout = ({ children }) => {
   return (
     <>
-      <Navbar />
+      <Navbar logoImageSrc="https://ui8-fleet-html.herokuapp.com/img/logo-dark.svg" />
       <Box>{children}</Box>
+      <Footer
+        logoImageSrc="https://ui8-fleet-html.herokuapp.com/img/logo-dark.svg"
+        copyright="Copyright 2021 @ Fleet"
+      />
     </>
   )
 }
