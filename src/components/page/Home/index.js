ac34bb85593c18da858dda4bff1c7b554a74e120
NamTruongcoder <hoangnamqt.2001@gmail.com>
2021-10-25 22:38:25 +0700

    feat(homepage)

---
+++ src/components/page/Home/index.js
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <div>Home page321312</div>
+}
+
+export default Home
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import { Button, Box, Text } from '@chakra-ui/react'
-
+import Home from '@/components/page/Home'
 export default function Index() {
-  return <Box></Box>
+  return <Home />
 }
