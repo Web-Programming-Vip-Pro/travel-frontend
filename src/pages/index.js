2ece149ec7b3ad73cc3bb1506b70b373c5ef6fd7
NamTruongcoder <hoangnamqt.2001@gmail.com>
2021-11-03 09:10:42 +0700

    fix(homepage):add all subcomponent to index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,26 @@
-import { Button, Box, Text } from '@chakra-ui/react'
-import Home from '@/components/page/Home'
-
+import { Box } from '@chakra-ui/react'
+import Hero from '@/components/page/Home/Hero'
+import Adventure from '@/components/page/Home/Adventure'
+import PlaceList from '@/components/page/Home/PlaceList'
+import BestAgencies from '@/components/page/Home/BestAgencies'
 export default function Index() {
   return (
     <>
-      <Home />
+      <Box px={{ mobile: '16px', tablet: '40px', desktop: '80px' }}>
+        <Hero
+          slogan="Air,sleep,dream"
+          description="Find and book a great experience"
+        />
+        <Adventure
+          title="Let's go on an adventure"
+          description="Find and book a great experience"
+        />
+        <PlaceList
+          title="Go somewhere"
+          description="Let's go on an Adventure"
+        />
+        <BestAgencies header="Best agencies of the" />
+      </Box>
     </>
   )
 }
