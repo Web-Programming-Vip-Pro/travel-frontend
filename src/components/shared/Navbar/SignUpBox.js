c5a3829e956f8b4258255e0b26003ab1a84adb5c
Thanh Minh <thanhgamedev@gmail.com>
2021-11-07 09:32:17 +0000

    chore(component-shared): update text

--- src/components/shared/Navbar/SignUpBox.js
+++ src/components/shared/Navbar/SignUpBox.js
@@ -13,7 +13,7 @@ const SignUpBox = () => {
   return (
     <Stack spacing="32px" align="center">
       <Stack spacing="12px" align="center">
-        <Text textStyle="headline-3">Sign Up on Travel</Text>
+        <Text textStyle="headline-3">Sign Up on Fleety</Text>
         <Text textStyle="body-2" color="neutrals.4">
           Use Your Email to Sign up
         </Text>
@@ -27,7 +27,7 @@ const SignUpBox = () => {
             placeholder="Enter your email"
             fontSize="14"
             _placeholder={{
-              fontSize: 14
+              fontSize: 14,
             }}
           />
           <InputRightElement height="100%" right="1">
@@ -49,7 +49,8 @@ const SignUpBox = () => {
         </InputGroup>
       </Stack>
       <Text textStyle="caption-2" fontWeight="bold">
-        Already have an account? <chakra.span color="primary.1">Login</chakra.span>
+        Already have an account?{' '}
+        <chakra.span color="primary.1">Login</chakra.span>
       </Text>
     </Stack>
   )
