dad1a3691ab99bde1f0b263894f45fa1e54cb4c5
Bui Tung <ductungbuinguyen@gmail.com>
2021-11-10 23:59:45 +0700

    feat(create property): UI of review card

--- src/pages/createproperty.js
+++ src/pages/createproperty.js
@@ -1,14 +1,35 @@
 import React from 'react'
-import { Flex, Box, Stack } from '@chakra-ui/react'
+import { Flex, Box, Text } from '@chakra-ui/react'
 import CreatePropertyForm from '@/components/createPropertyFeature/createPropertyForm'
-
+import SinglePlaceCard from '@/components/shared/SinglePlaceCard'
+const item = {
+  title: 'Entire serviced classy moutain house',
+  wifi: 'Free wifi',
+  breakfast: 'Breakfast included',
+  imgSrc: '/assets/citypage/Stay 1.jpg',
+  price: 548,
+  reviews: 12,
+  rate: 4.9,
+}
 const ListYourProperty = () => {
   return (
-    <Box px={{ base: '32px', tablet: '80px', desktop: '160px' }}>
-      <Box>
+    <Flex
+      p={{
+        base: '32px 32px 64px',
+        tablet: '80px 80px 136px',
+        desktop: '80px 160px 136px',
+      }}
+    >
+      <Box w="100%">
         <CreatePropertyForm />
       </Box>
-    </Box>
+      <Box ml="128px" display={{ base: 'none', desktop: 'block' }}>
+        <Text mb="40px" textStyle="body-1-bold">
+          Preview
+        </Text>
+        <SinglePlaceCard item={item} />
+      </Box>
+    </Flex>
   )
 }
 
