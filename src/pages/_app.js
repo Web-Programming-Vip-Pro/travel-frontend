cc5b0f27eb4052eb72c1bdd254a159bd75c04f72
Thanh Minh <thanhgamedev@gmail.com>
2021-11-11 05:53:40 +0000

    feat(authentication): verify user

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,8 +5,14 @@ import '@fontsource/inter/700.css'
 import { ChakraProvider } from '@chakra-ui/react'
 import { theme } from '@/theme/index'
 import DefaultLayout from '@/layouts/DefaultLayout'
+import { useUser } from '@/services/auth'
+import { useUserStore } from '@/store/user'
 
 function MyApp({ Component, pageProps }) {
+  const { user, isLoading, isError } = useUser()
+  if (!isLoading && !isError) {
+    useUserStore.getState().setUser(user)
+  }
   return (
     <ChakraProvider theme={theme}>
       <DefaultLayout>
