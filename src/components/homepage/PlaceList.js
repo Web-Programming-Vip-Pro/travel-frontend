672a7f3a966c061c31603e7f570a58f2bcb4a8b0
NamTruongcoder <hoangnamqt.2001@gmail.com>
2021-11-22 11:19:29 +0700

    feat(homepage):render API

--- src/components/homepage/PlaceList.js
+++ src/components/homepage/PlaceList.js
@@ -13,6 +13,8 @@ import Image from 'next/image'
 import UtilityNav from '@/components/shared/UtilityNav'
 import { Icon } from '@iconify/react'
 import { useState } from 'react'
+import Link from 'next/link'
+import { usePlaces } from '@/services/places'
 function PlaceCardList({ data, show }) {
   return (
     <SimpleGrid
@@ -23,147 +25,104 @@ function PlaceCardList({ data, show }) {
       overflow="hidden"
     >
       {data.map((item, index) => (
-        <Flex
-          key={index}
-          justify="center"
-          _hover={{
-            cursor: 'pointer',
-          }}
-        >
-          <Box
-            borderRadius="20px"
-            w="256px"
-            h="365px"
-            overflow="hidden"
-            bg="neutrals.8"
+        <Link href={`\place\/${item.id}`} key={index}>
+          <Flex
+            justify="center"
+            _hover={{
+              cursor: 'pointer',
+            }}
           >
-            <Image
-              src={item.imgSrc}
-              alt={item.title}
-              width={256}
-              height={228}
-            />
-            <Box p="20px">
-              <Flex justify="space-between" align="center">
-                <Flex direction="column">
-                  <Text textStyle="body-2-bold" color="neutrals.1">
-                    {item.title}
-                  </Text>
-                  <Text textStyle="caption-2" color="neutrals.3">
-                    {item.subTitle}
-                  </Text>
-                </Flex>
-                <Badge
-                  w="48px"
-                  h="26px"
-                  variant="outline"
-                  colorScheme="green"
-                  textAlign="center"
-                  lineHeight="26px"
-                  boxShadow="0 0 0 3px #58C27D;
+            <Box
+              borderRadius="20px"
+              w="256px"
+              h="365px"
+              overflow="hidden"
+              bg="neutrals.8"
+            >
+              <Image
+                src={item.images.cover}
+                alt={item.title}
+                width={256}
+                height={228}
+                unoptimized
+              />
+              <Box p="20px">
+                <Flex justify="space-between" align="center">
+                  <Flex direction="column">
+                    <Text textStyle="body-2-bold" color="neutrals.1">
+                      {item.title}
+                    </Text>
+                    <Text textStyle="caption-2" color="neutrals.3">
+                      {item.city.name}
+                    </Text>
+                  </Flex>
+                  <Badge
+                    w="48px"
+                    h="26px"
+                    variant="outline"
+                    colorScheme="green"
+                    textAlign="center"
+                    lineHeight="26px"
+                    boxShadow="0 0 0 3px #58C27D;
                   "
-                >
-                  ${item.price}
-                </Badge>
-              </Flex>
-              <Divider pt="16px" />
-              <Flex pt="16px">
-                <Text textStyle="caption-2" color="neutrals.4">
-                  {item.timeLine}
-                </Text>
-                <Spacer />
-                <Flex direction="row" align="center">
+                  >
+                    ${item.price}
+                  </Badge>
+                </Flex>
+                <Divider pt="16px" />
+                <Flex pt="16px" align="center" justify="center">
                   <Icon
                     icon="fluent:star-12-filled"
                     color="#FFD166"
                     width="12px"
                   />
                   <Text textStyle="caption-2-bold" color="neutrals-2" pl="4px">
-                    {item.rate}
+                    {item.stars}
                   </Text>
                 </Flex>
-              </Flex>
+              </Box>
             </Box>
-          </Box>
-        </Flex>
+          </Flex>
+        </Link>
       ))}
     </SimpleGrid>
   )
 }
 const PlaceList = ({ title, description }) => {
-  const listPlace = [
-    {
-      title: 'The grand resort',
-      subTitle: 'Karinesie',
-      imgSrc: '/assets/homepage/Place Snow.png',
-      price: 548,
-      timeLine: '8:00 - 18.00',
-      rate: 4.9,
-    },
-    {
-      title: 'The grand resort',
-      subTitle: 'Karinesie',
-      imgSrc: '/assets/homepage/Place Beach.png',
-      price: 548,
-      timeLine: '8:00 - 18.00',
-      rate: 4.9,
-    },
-    {
-      title: 'The grand resort',
-      subTitle: 'Karinesie',
-      imgSrc: '/assets/homepage/Place Field.png',
-      price: 548,
-      timeLine: '8:00 - 18.00',
-      rate: 4.9,
-    },
-    {
-      title: 'The grand resort',
-      subTitle: 'Karinesie',
-      imgSrc: '/assets/homepage/Place Fog.png',
-      price: 548,
-      timeLine: '8:00 - 18.00',
-      rate: 4.9,
-    },
-    {
-      title: 'The grand resort',
-      subTitle: 'Karinesie',
-      imgSrc: '/assets/homepage/Place Wave.png',
-      price: 548,
-      timeLine: '8:00 - 18.00',
-      rate: 4.9,
-    },
-    {
-      title: 'The grand resort',
-      subTitle: 'Karinesie',
-      imgSrc: '/assets/homepage/Place Bay.png',
-      price: 548,
-      timeLine: '8:00 - 18.00',
-      rate: 4.9,
-    },
-    {
-      title: 'The grand resort',
-      subTitle: 'Karinesie',
-      imgSrc: '/assets/homepage/Place Snow.png',
-      price: 548,
-      timeLine: '8:00 - 18.00',
-      rate: 4.9,
-    },
-    {
-      title: 'The grand resort',
-      subTitle: 'Karinesie',
-      imgSrc: '/assets/homepage/Place Sky.png',
-      price: 548,
-      timeLine: '8:00 - 18.00',
-      rate: 4.9,
-    },
-  ]
+  const [page, setPage] = useState(0)
+  const [limit, setLimit] = useState(6)
+  //  order = 'recent' | 'rating' | 'max-price' | 'min-price'
+  const [order, setOrder] = useState('recent')
+  const [type, setType] = useState(0)
+  const { places, isLoading, error } = usePlaces(page, limit, order, type)
+  console.log(page, limit, order, type)
+  const handleOrder = (item) => {
+    let order
+    switch (item) {
+      case 'Recently Added':
+        order = 'recent'
+        break
+      case 'Most Ratings':
+        order = 'rating'
+        break
+      case 'High Price':
+        order = 'max-price'
+        break
+      case 'Low Price':
+        order = 'min-price'
+        break
+    }
+    setOrder(order)
+  }
+  const handleType = (index) => {
+    setType(index)
+  }
   const [show, setShow] = useState(false)
   return (
-    <Box minH={{ mobile: '2858px', tablet: '1446px' }}>
+    <Box h="fit-content">
       <Box
         bgColor="neutrals.7"
         borderRadius={{ tablet: '20px' }}
-        minH="1286px"
         overflow="hidden"
         mx={{ mobile: '-16px', tablet: '0' }}
         px={{ mobile: '16px', tablet: '40px', desktop: '80px' }}
@@ -184,18 +143,27 @@ const PlaceList = ({ title, description }) => {
         >
           <Text>{description}</Text>
         </Box>
-        <UtilityNav />
-        <PlaceCardList data={listPlace} show={show} />
-        <Flex justify="center">
-          <Button
-            variant="outline"
-            border="2px"
-            my="64px"
-            onClick={() => setShow(!show)}
-          >
-            {show ? 'Hide' : 'View All'}
-          </Button>
-        </Flex>
+        <UtilityNav triggerOrder={handleOrder} triggerType={handleType} />
+        {isLoading ? (
+          <Stack justify="center" align="center">
+            <Icon icon="eos-icons:bubble-loading" fontSize="50px" />
+          </Stack>
+        ) : (
+          <Box>
+            {/* <PlaceCardList data={listPlace} show={show} /> */}
+            <PlaceCardList data={places} show={show} />
+            <Flex justify="center">
+              <Button
+                variant="outline"
+                border="2px"
+                my="64px"
+                onClick={() => setShow(!show)}
+              >
+                {show ? 'Hide' : 'View All'}
+              </Button>
+            </Flex>
+          </Box>
+        )}
       </Box>
     </Box>
   )
