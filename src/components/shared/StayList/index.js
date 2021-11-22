bb8eb53cdbe42856ccb052647961deb0831c0a8b
NamTruongcoder <hoangnamqt.2001@gmail.com>
2021-11-22 20:49:06 +0700

    feat(citypage):render API for citypage

--- src/components/shared/StayList/index.js
+++ src/components/shared/StayList/index.js
@@ -23,90 +23,99 @@ function PlaceCardList({ listPlace }) {
       spacingY="32px"
       overflow="hidden"
     >
-      {listPlace.map((item, index) => (
-        <Flex key={index} justify="space-around" _hover={{ cursor: 'pointer' }}>
-          <Box
-            borderRadius="20px"
-            w={{ tablet: '266px', desktop: '352px' }}
-            h={{ tablet: '411px', desktop: '417px' }}
-            overflow="hidden"
-            bg="neutrals.8"
+      {listPlace &&
+        listPlace.map((item, index) => (
+          <Flex
+            key={index}
+            justify="space-around"
+            _hover={{ cursor: 'pointer' }}
           >
             <Box
-              width={{ mobile: '311px', tablet: '266px', desktop: '352px' }}
-              height={{ mobile: '240px', desktop: '240px' }}
-              position="relative"
+              borderRadius="20px"
+              w={{ tablet: '266px', desktop: '352px' }}
+              h={{ tablet: '411px', desktop: '417px' }}
+              overflow="hidden"
+              bg="neutrals.8"
             >
-              <Image
-                src={item.images.cover}
-                alt={item.title}
-                layout="fill"
-                objectFit="cover"
-                position="absolute"
-                unoptimized
-              />
-              <Badge
-                position="absolute"
-                top="16px"
-                left="16px"
-                textStyle="hairline-2"
-                bg="neutrals.8"
-                p="8px"
+              <Box
+                width={{ mobile: '311px', tablet: '266px', desktop: '352px' }}
+                height={{ mobile: '240px', desktop: '240px' }}
+                position="relative"
               >
-                Stay
-              </Badge>
-            </Box>
-
-            <Box p="20px">
-              <Flex justify="space-between" align="center" pb="8px">
-                <Text
-                  textStyle="body-2-bold"
-                  color="neutrals.1"
-                  w="186px"
-                  wordBreak="break-all"
-                >
-                  {item.title}
-                </Text>
-
+                <Image
+                  src={item.images.cover}
+                  alt={item.title}
+                  layout="fill"
+                  objectFit="cover"
+                  position="absolute"
+                  unoptimized
+                />
                 <Badge
-                  w="48px"
-                  h="26px"
-                  variant="outline"
-                  colorScheme="green"
-                  textAlign="center"
-                  lineHeight="26px"
-                  boxShadow="0 0 0 3px #58C27D;
-                    "
+                  position="absolute"
+                  top="16px"
+                  left="16px"
+                  textStyle="hairline-2"
+                  bg="neutrals.8"
+                  p="8px"
                 >
-                  ${item.price}
+                  Stay
                 </Badge>
-              </Flex>
-              <HStack textStyle="caption-2" color="neutrals.4">
-                <Icon icon="icon-park-outline:router" />
-                <Text>Free wifi</Text>
-                <Icon icon="ion:fast-food-outline" />
-                <Text>Breakfast included</Text>
-              </HStack>
-              <Divider pt="16px" />
-              <Flex pt="16px" justify="center">
-                <Flex direction="row" align="center" pr="8px">
-                  <Icon
-                    icon="fluent:star-12-filled"
-                    color="#FFD166"
-                    width="12px"
-                  />
-                  <Text textStyle="caption-2-bold" color="neutrals-2" pl="4px">
-                    {item.rate}
+              </Box>
+
+              <Box p="20px">
+                <Flex justify="space-between" align="center" pb="8px">
+                  <Text
+                    textStyle="body-2-bold"
+                    color="neutrals.1"
+                    w="186px"
+                    wordBreak="break-all"
+                  >
+                    {item.title}
+                  </Text>
+
+                  <Badge
+                    w="48px"
+                    h="26px"
+                    variant="outline"
+                    colorScheme="green"
+                    textAlign="center"
+                    lineHeight="26px"
+                    boxShadow="0 0 0 3px #58C27D;
+                    "
+                  >
+                    ${item.price}
+                  </Badge>
+                </Flex>
+                <HStack textStyle="caption-2" color="neutrals.4">
+                  <Icon icon="icon-park-outline:router" />
+                  <Text>Free wifi</Text>
+                  <Icon icon="ion:fast-food-outline" />
+                  <Text>Breakfast included</Text>
+                </HStack>
+                <Divider pt="16px" />
+                <Flex pt="16px" justify="center">
+                  <Flex direction="row" align="center" pr="8px">
+                    <Icon
+                      icon="fluent:star-12-filled"
+                      color="#FFD166"
+                      width="12px"
+                    />
+                    <Text
+                      textStyle="caption-2-bold"
+                      color="neutrals-2"
+                      pl="4px"
+                    >
+                      {item.rate}
+                    </Text>
+                  </Flex>
+                  <Text textStyle="caption-2" color="neutrals.4">
+                    ({item.reviews} reviews)
                   </Text>
                 </Flex>
-                <Text textStyle="caption-2" color="neutrals.4">
-                  ({item.reviews} reviews)
-                </Text>
-              </Flex>
+              </Box>
             </Box>
-          </Box>
-        </Flex>
-      ))}
+          </Flex>
+        ))}
     </SimpleGrid>
   )
 }
@@ -145,10 +154,7 @@ const StayList = ({ children, listPlace, isLoading, triggerPage }) => {
                 variant="outline"
                 border="2px"
                 my="64px"
-                onClick={() => {
-                  setShow(!show)
-                  triggerPage()
-                }}
+                onClick={triggerPage}
               >
                 {show ? 'Hide' : 'View All'}
               </Button>
